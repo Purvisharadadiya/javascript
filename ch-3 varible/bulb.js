de6c1bd251a3ff85134191267b0ecda45fa748f6
purvisharadadiya <purva.radadiya3110@gmail.com>
2025-01-29 15:43:31 +0530

    javascrpi

---
+++ ch-3 varible/bulb.js
@@ -0,0 +1,11 @@
+function bulbon()
+{
+     let bulbobj = document.querySelector(".bulboff");
+     bulbobj.setAttribute("src","img/on bulb.png")
+      
+}
+function bulboff()
+{
+    let bulbobj = document.querySelector(".bulboff");
+    bulbobj.setAttribute("src","img/off bulb.png")
+}    
